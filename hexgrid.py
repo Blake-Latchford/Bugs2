24e8e48dbc57088a14f36f63c10f45d2e3abcfd9
Blake-Latchford <blake.latchford@gmail.com>
2017-01-14 01:06:56 -0500

    Add registration to hexgrid.

--- hexgrid.py
+++ hexgrid.py
@@ -4,10 +4,18 @@ from hexcell import HexCell
 
 class HexGrid:
     def __init__(self):
-        self.populated_cells = {}
+        self._populated_cells = {}
 
     def get_cell(self, q, r):
+        """Get the cell at the specified coordinates. If no cell is registered
+        at that location, create a temporary new cell."""
+        
         coords = (q, r)
-        if coords in self.populated_cells:
-            return self.populated_cells[coords]
+        if coords in self._populated_cells:
+            return self._populated_cells[coords]
         return HexCell(self, q, r)
+    
+    def register_cell(self, hex_cell):
+        """Register a hex cell to be retained in the grid."""
+        
+        self._populated_cells[(hex_cell.q, hex_cell.r)] = hex_cell
--- test/test_hexgrid.py
+++ test/test_hexgrid.py
@@ -7,3 +7,18 @@ class HexGridTestCase(unittest.TestCase):
     
     def setUp(self):
         self.hex_grid = hexgrid.HexGrid()
+        self.origin_cell = self.hex_grid.get_cell(0, 0)
+        self.hex_grid.register_cell(self.origin_cell)
+    
+    def test_find_registered_cell(self):
+        self.assertIs(
+            self.hex_grid.get_cell(0, 0),
+            self.origin_cell)
+        
+    def test_dont_find_registered_cell(self):
+        first_unregistered_cell = self.hex_grid.get_cell(0, 1)
+        second_unregistered_cell = self.hex_grid.get_cell(0, 1)
+        
+        self.assertEqual(first_unregistered_cell, second_unregistered_cell)
+        self.assertIsNot(first_unregistered_cell, second_unregistered_cell)
+        
