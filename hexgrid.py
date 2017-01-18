2720dbbe11c2265d1b53f10c5af07ffc276fb915
Blake-Latchford <blake.latchford@gmail.com>
2017-01-18 07:41:16 -0500

    Remove hexcell breadth first search.

--- hexgrid.py
+++ hexgrid.py
@@ -36,36 +36,3 @@ class HexGrid:
 
     def reset(self):
         self._registered_cells = {}
-
-    def breadth_first_search(self, start, max_distance, filter_function=None):
-        """Do a breadth first search from start and going max_distance
-        hexes away.
-
-        Arguments:
-            start - HexCell at which to start the search.
-            max_distance - Maximum Manhattan distance to search.
-            filter_function - If specified, then can be used to prevent hexes
-                from being accepted in a search. Expected signature is:
-                filter_function(hex_cell, distance) -> bool
-                If the return value is true, hex_cell is added to the results.
-                distance is the Manhattan distance from start.
-        """
-        search_results = []
-        previous_distance_result = [start]
-        visited = set([start])
-
-        for search_results_index in range(max_distance):
-            distance = search_results_index + 1
-            current_distance_result = []
-
-            for hex_cell in previous_distance_result:
-                for neighbor in hex_cell.get_neighbors(self):
-                    if (neighbor not in visited and
-                            (not filter_function or filter_function(neighbor, distance))):
-                        current_distance_result.append(neighbor)
-                        visited.add(neighbor)
-
-            search_results.append(current_distance_result)
-            previous_distance_result = current_distance_result
-
-        return search_results
