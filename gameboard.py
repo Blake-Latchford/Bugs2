515fbb7378d1a003ec719a15c627b585c69328d1
Blake-Latchford <blake.latchford@gmail.com>
2017-01-22 11:48:24 -0500

    Refactor unplaced pieces to only list the next of a type, instead of all possible.

--- gameboard.py
+++ gameboard.py
@@ -21,12 +21,13 @@ class GameBoard(hexgrid.HexGrid):
 
         for color in piece.Color:
             for piece_type, piece_count in self._piece_type_counts.items():
-                for piece_number in range(piece_count):
+                if piece_count > 0:
                     self._unplaced_pieces.add(
-                        piece.Piece(piece_type, color, piece_number))
+                        piece.Piece(piece_type, color, 0))
 
     def place(self, new_piece):
         local_instance = self._get_piece(new_piece)
+        assert local_instance
 
         available_moves = local_instance.get_moves(self)
         if self.get_cell(new_piece.q, new_piece.r) not in available_moves:
@@ -40,38 +41,9 @@ class GameBoard(hexgrid.HexGrid):
         Storage consistency assumptions are validated however."""
 
         local_instance = self._get_piece(new_piece)
-
-        # Make sure this new_piece is valid to place.
-        if local_instance is new_piece:
-            raise ValueError(
-                "Unable to place new_piece already on gameboard:" + str(new_piece))
-
-        if not local_instance:
-            raise ValueError(
-                "Piece not available for placement:" + str(new_piece))
-
-        if math.isnan(new_piece.q) or math.isnan(new_piece.r):
-            raise ValueError("Piece does not have coordinates specified:" +
-                             str(new_piece))
-
-        # Clean up the old location
-        if local_instance in self._unplaced_pieces:
-            self._unplaced_pieces.remove(local_instance)
-
-        if local_instance in self._placed_pieces:
-            self._placed_pieces.remove(local_instance)
-            self.unregister_cell(local_instance)
-
-            if local_instance.above:
-                self.register_cell(local_instance.above)
-
-        bottom_piece = self.get_cell(new_piece.q, new_piece.r)
-        if piece.Piece.is_piece(bottom_piece):
-            self.unregister_cell(bottom_piece)
-            new_piece.above = bottom_piece
-
-        self.register_cell(new_piece)
-        self._placed_pieces.add(new_piece)
+        self._validate_piece(new_piece, local_instance)
+        self._remove_replaced_piece(local_instance)
+        self._register_new_piece(new_piece)
 
     def _get_piece(self, piece):
         """Get a piece based on its color, type and number."""
@@ -88,6 +60,58 @@ class GameBoard(hexgrid.HexGrid):
                 return placed_piece
 
         return None
+
+    def _validate_piece(self, new_piece, local_instance):
+        # Make sure this new_piece is valid to place.
+        if local_instance is new_piece:
+            raise ValueError(
+                "Unable to place new_piece already on game board:" +
+                str(new_piece))
+
+        if not local_instance:
+            raise ValueError(
+                "Piece not available for placement:" + str(new_piece))
+
+        if math.isnan(new_piece.q) or math.isnan(new_piece.r):
+            raise ValueError("Piece does not have coordinates specified:" +
+                             str(new_piece))
+
+    def _remove_replaced_piece(self, replaced_piece):
+        self._remove_unplaced(replaced_piece)
+        self._remove_placed(replaced_piece)
+
+    def _remove_unplaced(self, unplaced_piece):
+        if unplaced_piece not in self._unplaced_pieces:
+            return
+
+        next_unplaced_piece = piece.Piece(
+            unplaced_piece.piece_type,
+            unplaced_piece.color,
+            unplaced_piece.piece_number + 1)
+
+        max_piece_count = self._piece_type_counts[unplaced_piece.piece_type]
+        if next_unplaced_piece.piece_number < max_piece_count:
+            self._unplaced_pieces.add(next_unplaced_piece)
+        self._unplaced_pieces.remove(unplaced_piece)
+
+    def _remove_placed(self, placed_piece):
+        if placed_piece not in self._placed_pieces:
+            return
+
+        self._placed_pieces.remove(placed_piece)
+        self.unregister_cell(placed_piece)
+
+        if placed_piece.above:
+            self.register_cell(placed_piece.above)
+
+    def _register_new_piece(self, new_piece):
+        bottom_piece = self.get_cell(new_piece.q, new_piece.r)
+        if piece.Piece.is_piece(bottom_piece):
+            self.unregister_cell(bottom_piece)
+            new_piece.above = bottom_piece
+
+        self.register_cell(new_piece)
+        self._placed_pieces.add(new_piece)
 
     def get_pieces(self, color=None):
         pieces = set(self.get_placed_pieces(color))
